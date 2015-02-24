ac41f6195965d57d104ee58f1b90ef6e543d4d9a
Ilya Lovriakov <ilya.lovriakov@booking.com>
2015-02-24 09:08:42 +0100

    some markup for single experiment

--- app/views/experiment/index.js
+++ app/views/experiment/index.js
@@ -2,6 +2,7 @@ import laborantView from 'tracking-server-ember/views/laborant-view';
 
 export default laborantView.extend({
   didInsertElement: function() {
+  	this.$('ul.tabs').tabs();
     this.$('.modal-trigger').leanModal();
     this._super();
   }
