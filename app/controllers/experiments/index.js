fcbb00befec6c0ef59f31571c71a7aade8d0be0b
Ilya Lovriakov <ilya.lovriakov@booking.com>
2015-02-24 09:22:47 +0100

    computed property in controller

--- app/controllers/experiments/index.js
+++ app/controllers/experiments/index.js
@@ -2,5 +2,8 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   sortProperties: ['dateCreated'],
-  sortAscending: true // false for descending
+  sortAscending: true, // false for descending
+  totalText: function() {
+    return 'All Experiments' + ' (' + this.get('model').content.length + ')';
+  }.property('model')
 });
