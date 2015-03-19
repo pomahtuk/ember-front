e63c9259b2fead57d02853926b6b90010f9bd8fe
pomahtuk <pman89@ya.ru>
2015-03-19 23:48:52 +0100

    auth all requsts to server

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,11 +2,11 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   host: 'http://localhost:3000',
-  defaultSerializer: DS.RESTSerializer.extend({
-    serializeId: function(id) {
-      return id.toString();
-    }
-  })
+  ajax: function(url, method, hash) {
+    hash.crossDomain = true;
+    hash.xhrFields = {withCredentials: true};
+    return this._super(url, method, hash);
+  }
 });
 
 // export default DS.FixtureAdapter;
